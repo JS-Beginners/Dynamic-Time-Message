fa938273cab9adfd1b3a184b89c82f30de5addce
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2020-12-27 15:34:25 +0000

    Update app.js
    If the current time is 00:01 the program will show Good morning when it should be Good evening.
    Usually:
    Good Morning    5:00 AM — 11:59 AM
    Good Afternoon        12:00 PM — 4:59 PM
    Good Evening        5:00 PM — 4:59 AM
    So, I suggest changing the IF clauses as proposed.

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const messageArea = document.querySelector('#message');
 //add a click event to the button
 button.addEventListener('click', function(){
 //check for morning, noon, or evening
-if (hour > 17){
+if (hour >=  17 || hour <5){
     messageArea.innerHTML = `<h1>Good evening!</h1>`;
-} else if (hour > 12){
+} else if (hour >= 12 || hour< 17){
     messageArea.innerHTML = `<h1>Good afternoon!</h1>`;
 } else {
     messageArea.innerHTML = `<h1>Good morning!</h1>`;
